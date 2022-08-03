8a5fc8f19fc7e959c2eb30fb0afd0a24524093dc
Firstname Lastname <e-mail address>
2022-08-02 19:49:06 -0700

    added struct for RSVPs

---
+++ contracts/web3RSVP.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Web3RSVP {
+  struct CreateEvent {
+    bytes32 eventId;
+    string eventDataCID;
+    address eventOwner;
+    uint256 eventTimestamp;
+    uint256 deposit;
+    uint256 maxCapacity;
+    address[] confirmedRSVPs;
+    address[] claimedRSVPs;
+    bool paidOut;
+  }
+
+  mapping(bytes32 => CreateEvent) public idToEvent;
+}
