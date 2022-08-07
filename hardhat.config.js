4849473b2f6564619ce32e1417d32ceb3eb2783a
Firstname Lastname <e-mail address>
2022-08-07 11:42:55 -0700

    Switched to deploying via Alchemy instead of Infura

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */	
-module.exports = {
-	solidity: "0.8.9",
-    networks: {
-    	hardhat: {
-      		chainId: 1337,
-    	},
-	mumbai: {
-		url: process.env.STAGING_INFURA_URL,
-		accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
-		gas: 2100000,
-		gasPrice: 8000000000,
+// const { API_URL, PRIVATE_KEY } = process.env;
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+  
+	for (const account of accounts) {
+	  console.log(account.address);
 	}
-	}
-};
+  });
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+	solidity: "0.8.4",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		mumbai: {
+			url: process.env.API_URL,
+			accounts: [`0x${process.env.PRIVATE_KEY}`],
+			gas: 2100000,
+			gasPrice: 8000000000,
+		},
+	},
+  };
